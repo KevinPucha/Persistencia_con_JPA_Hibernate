d65370ab89d47388926b8a8957ef60496334d102
Kevin Pucha <kevinpucha197a@gmail.com>
2023-06-23 14:03:08 -0500

    Creting methods to search Productos

--- src/main/java/com/latam/alura/tienda/prueba/RegistroDeProducto.java
+++ src/main/java/com/latam/alura/tienda/prueba/RegistroDeProducto.java
@@ -1,6 +1,7 @@
 package com.latam.alura.tienda.prueba;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 import javax.persistence.EntityManager;
 
@@ -13,28 +14,33 @@ import com.latam.alura.tienda.utils.JPAUtils;
 public class RegistroDeProducto {
 
 	public static void main(String[] args) {
-		Categoria celulares = new Categoria("CELULAR");
-		Categoria newCallToCelulares = new Categoria("CELULAR");
-	
+		registrarProducto();
 		EntityManager em = JPAUtils.getEntityManager();
-				
+		ProductoDao productoDao = new ProductoDao(em);
+		Producto producto = productoDao.consultaPorId(1l);
+		System.out.println(producto.getNombre());
+		
+		List<Producto> productos = productoDao.consultarTodos();
+		productos.forEach(prod -> System.out.println(prod.getDescripcion()));
+
+	}
+
+	private static void registrarProducto() {
+		Categoria celulares = new Categoria("CELULAR");
+		Producto celular = new Producto("Samsung","Teléfono usado",new BigDecimal("1000"),celulares);
+
+		EntityManager em = JPAUtils.getEntityManager();
+
+		ProductoDao productoDao = new ProductoDao(em);
+		CategoriaDao categoriaDao = new CategoriaDao(em);
+
 		em.getTransaction().begin();
-		
-		em.persist(celulares);
-		celulares.setNombre("LIBROS");
-		
-		em.flush();
-		em.clear();
-		
-		newCallToCelulares = em.merge(celulares);
-		newCallToCelulares.setNombre("SOFTWARES");
-		em.flush();
-		em.clear();
-		
-		newCallToCelulares = em.merge(newCallToCelulares);
-		em.remove(newCallToCelulares);
-		em.flush();
-		
+
+		categoriaDao.guardar(celulares);
+		productoDao.guardar(celular);
+
+		em.getTransaction().commit(); // This send the values to the DB
+		em.close();
 	}
 
 }
