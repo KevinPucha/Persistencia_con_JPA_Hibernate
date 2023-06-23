cc1df797c3377804d8daad07a91f26ee605cc9e3
Kevin Pucha <kevinpucha197a@gmail.com>
2023-06-23 10:33:53 -0500

    Creating date and category attributes, and new constructors

--- src/main/java/com/latam/alura/tienda/modelo/Producto.java
+++ src/main/java/com/latam/alura/tienda/modelo/Producto.java
@@ -1,6 +1,7 @@
 package com.latam.alura.tienda.modelo;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -16,13 +17,23 @@ public class Producto {
 	
 	@Id //Indicate that this is the Primary Key
 	@GeneratedValue(strategy = GenerationType.IDENTITY) // Indicate that the responsability of generate the Id is of the DB and not from the user 
-	
 	private Long id;
 	@Column(name="nombre") // Changing the name of the below column
 	private String nombre;
 	private String descripcion;
 	private BigDecimal precio;
+	private LocalDate fechaDeRegistro = LocalDate.now();
+	private Categoria categoria;
 	
+	public Producto () {
+	}
+	public Producto(String nombre, String descripcion, BigDecimal precio, Categoria categoria) {
+		this.nombre = nombre;
+		this.descripcion = descripcion;
+		this.precio = precio;
+		this.categoria = categoria;
+	}
+
 	public Long getId() {
 		return id;
 	}
